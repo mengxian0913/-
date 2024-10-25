2d809299308bec7a775b1ac1d5423547abcd1f01
mengxain0913 <mengxian0913@gmail.com>
2024-10-25 21:24:25 +0800

    finished: lab01

--- lab01/p1.cpp
+++ lab01/p1.cpp
@@ -6,11 +6,41 @@
 using namespace std;
 using namespace cv;
 
-const string imageURL = "./images/grayduck.png";
+const string imageURL = "./images/me.JPG";
+const string nameURL = "./images/name.png";
 
 void getImageInfo(Mat image) {
   cout << "image info: \nheight: " << image.rows << "\nwidth: " << image.cols << "\n";
   cout << "------------------\n";
+  return;
+}
+
+
+// 姓名烙印
+void writeName(Mat &image) {
+  Mat nameImage   = imread(nameURL); // 打開姓名圖片檔案 
+  int nameHeight  = nameImage.rows; // 取得高度
+  int nameWidth   = nameImage.cols; // 取得寬度
+  int startHeight = image.rows - nameHeight; // 取得 y 軸起始位置
+  int startWidth  = image.cols - nameWidth; // 取得 x 軸起始位置
+  /* 
+   * 遍歷 image 的 pixel 從 startHeight 到底和從 startWidth 到底
+   * 並且將 nameImage 為白色的地方烙印在對應的 image 座標上。
+  */
+  for(int i = startHeight, ni = 0; ni < nameHeight; i++, ni++) {
+    for(int j = startWidth, nj = 0; nj < nameWidth; j++, nj++) {
+      Vec3b &imageColor = image.at<Vec3b>(i, j); // 取得該座標的顏色
+      Vec3b &nameColor = nameImage.at<Vec3b>(ni, nj); // 取得該座標的顏色
+      if ( (int)nameColor[0] > 200 && (int)nameColor[1] > 200 && (int)nameColor[2] > 200 ) {
+        for(int k = 0; k < 3; k++) {
+          imageColor[k] = nameColor[k];
+        }
+      } 
+    }
+  }
+
+  getImageInfo(nameImage); // 打印圖片的相關資訊
+
   return;
 }
 
@@ -20,29 +50,32 @@ double getFilterPixel(int currentI, int currentJ, int currentK, int n, int m, Ma
   for(int i = 0; i < n; i++, currentI++) {
     for(int j = 0; j < m; j++, currentJ++) {
       Vec3b color = background.at<Vec3b>(currentI, currentJ);
+      // 把從 (0, 0) 到 (n, m) 對應到 (currentI, currentJ) 的 color[currentK] ( 可能是 r/g/b ) 計算相乘累加
       calRes += ( color[currentK] * kernel[i][j] );
     }
   }
-  
   return calRes;
 }
 
 Mat conv(Mat image, int n, int m, vector<vector<double>> &kernel, double bias, int padding) {
 
+  // 判斷高度和寬度是否合理 ( 不能為偶數 )
   if ( !(n & 1) || !( m & 1 ) ) {
     cerr << "Invaild value of n or m\n";
     exit(0);
   }
 
-  int height = image.rows;
-  int width = image.cols;
-  Mat background;
-  Mat resultImage;
+  int height = image.rows; // 取得 image 高度
+  int width = image.cols; // 取得 image 寬度
+  Mat background; // 創建一個圖片作為加上 padding 後的圖片
+  Mat resultImage; // 儲存過濾後的結果
 
+  // 如果不需要加 padding
   if ( padding == -1 ) {
-    background = Mat(height, width, CV_8UC3);
-    resultImage = Mat(height - ( n - 1 ), width - (m - 1), CV_8UC3);
+    background = Mat(height, width, CV_8UC3); // 設定 background 寬高。
+    resultImage = Mat(height - ( n - 1 ), width - (m - 1), CV_8UC3); // 設定 resultImage 寬高。
 
+    // 把 image 的 color 塞到 background
     for(int i = 0; i < image.rows; i++) {
       for(int j = 0; j < image.cols; j++) {
         Vec3b imageColor = image.at<Vec3b>(i, j);
@@ -53,9 +86,10 @@ Mat conv(Mat image, int n, int m, vector<vector<double>> &kernel, double bias, i
       }
     }
   } else {
-    background = Mat(height + ( n - 1 ), width + ( m - 1 ), CV_8UC3, Scalar(padding, padding, padding));
-    resultImage = Mat(height, width, CV_8UC3);
+    background = Mat(height + ( n - 1 ), width + ( m - 1 ), CV_8UC3, Scalar(padding, padding, padding)); // 設定 background 的寬高。
+    resultImage = Mat(height, width, CV_8UC3); // 設定 resultImage 的寬高。
 
+    // 把 image 的 color 塞到 backgorund 對應的座標
     for(int i = 0, bi = (n - 1) / 2; i < image.rows; i++, bi++) {
       for(int j = 0, bj = ( m - 1 ) / 2; j < image.cols; j++, bj++) {
         Vec3b imageColor = image.at<Vec3b>(i, j);
@@ -69,10 +103,10 @@ Mat conv(Mat image, int n, int m, vector<vector<double>> &kernel, double bias, i
 
 
   // ------------------------------------------------------------------
-
+  // 計算 background 和 kernel 過濾後的結果塞到 resultImage
+  // getFilterPixel 會計算遮罩內所有的 r/g/b 的平均
   for(int i = 0; i <= background.rows - n; i++) {
     for(int j = 0; j <= background.cols - m; j++) {
-      double filterColor = 0;
       Vec3b &color = resultImage.at<Vec3b>(i, j);
       for(int k = 0; k < 3; k++) {
         color[k] = getFilterPixel(i, j, k, n, m, background, kernel) + bias;
@@ -109,11 +143,15 @@ int main() {
   getImageInfo(image);
   // --------------------------
 
-  int n, m;
+  int n, m, bias, padding;
   cout << "input n: ";
   cin >> n;
   cout << "input m: ";
   cin >> m;
+  cout << "input bias: ";
+  cin >> bias;
+  cout << "input padding: ";
+  cin >> padding;
   vector<vector<double>> kernel(n, vector<double>(m, 0));
   cout << "input kernel:\n";
   for(int i = 0; i < n; i++) {
@@ -123,9 +161,12 @@ int main() {
   }
   
   imshow("origin image", image);
-  Mat resultImage = conv(image, n, m, kernel, 0, 1);
+  Mat resultImage = conv(image, n, m, kernel, bias, padding);
+  
+  writeName(resultImage);
   
   getImageInfo(resultImage);
+  // imwrite("./result/conv_me_average.png", resultImage); // 寫入結果
   imshow("result image", resultImage);
   
   waitKey(0); // 等待按鍵
--- lab01/p2.cpp
+++ lab01/p2.cpp
@@ -7,7 +7,8 @@ using namespace cv;
 
 #define INF (double)(1e9)
 
-const string imageURL = "./images/duck.jpeg";
+const string imageURL = "./images/otters.jpg";
+const string nameURL = "./images/name2.png";
 
 void getImageInfo(Mat image) {
   cout << "image info: \nheight: " << image.rows << "\nwidth: " << image.cols << "\n";
@@ -15,8 +16,32 @@ void getImageInfo(Mat image) {
   return;
 }
 
+
+void writeName(Mat &image) {
+  Mat nameImage   = imread(nameURL); 
+  int nameHeight  = nameImage.rows;
+  int nameWidth   = nameImage.cols;
+  int startHeight = image.rows - nameHeight;
+  int startWidth  = image.cols - nameWidth;
+  for(int i = startHeight, ni = 0; ni < nameHeight; i++, ni++) {
+    for(int j = startWidth, nj = 0; nj < nameWidth; j++, nj++) {
+      Vec3b &imageColor = image.at<Vec3b>(i, j);
+      Vec3b &nameColor = nameImage.at<Vec3b>(ni, nj);
+      if ( (int)nameColor[0] > 200 && (int)nameColor[1] > 200 && (int)nameColor[2] > 200 ) {
+        for(int k = 0; k < 3; k++) {
+          imageColor[k] = nameColor[k];
+        }
+      } 
+    }
+  }
+  getImageInfo(nameImage);
+
+  return;
+}
+
+
 double getMaxPixel(int currentI, int currentJ, int n, Mat image) {
-  double maxColor = -INF;
+  double maxColor = -INF; // 最大的 pixel
   for(int i = 0; i < n; i++, currentI++) {
     for(int j = 0; j < n; j++, currentJ++) {
       Vec3b color = image.at<Vec3b>(currentI, currentJ);
@@ -24,8 +49,8 @@ double getMaxPixel(int currentI, int currentJ, int n, Mat image) {
       for(int k = 0; k < 3; k++) {
         averageColor += (double)(color[k]);
       }
-      averageColor /= 3.0;
-      maxColor = max(maxColor, averageColor);
+      averageColor /= 3.0; // 取得 r/g/b 的平均
+      maxColor = max(maxColor, averageColor); // 計算最大的 maxColor
     }
   }
   return maxColor;
@@ -33,7 +58,7 @@ double getMaxPixel(int currentI, int currentJ, int n, Mat image) {
 
 
 double getAveragePixel( int currentI, int currentJ, int n, Mat image ) {
-  double avgColor = 0;
+  double avgColor = 0; // pixel 平均的 color 
   for(int i = 0; i < n; i++, currentI++) {
     for(int j = 0; j < n; j++, currentJ++) {
       Vec3b color = image.at<Vec3b>(currentI, currentJ);
@@ -41,23 +66,28 @@ double getAveragePixel( int currentI, int currentJ, int n, Mat image ) {
       for(int k = 0; k < 3; k++) {
         averageColor += (double)(color[k]);
       }
-      averageColor /= 3.0;
+      averageColor /= 3.0; // get average of r/g/b
       avgColor += averageColor;
     }
   }
 
-  avgColor /= ( n * n );
+  avgColor /= ( n * n ); // get average of matrix n * n
   return avgColor;
 }
 
 Mat pool(Mat image, int n, int stride, int type) {
-  int height = image.rows;
-  int width = image.cols;
-  vector<vector<vector<double>>> resultVec;
+  int height = image.rows; // 取得 image 的高度
+  int width  = image.cols; // 取得 image 的寬度
+  vector<vector<vector<double>>> resultVec; 
+  /*
+   * 創建一型態為 3 維 vector 的 resultVec 來儲存結果圖片需要的資訊。
+   * 之後會將它轉成 Mat 型態。
+   * */
   for(int i = 0; i <= height - n; i += stride) {
     vector<vector<double>> colColor;
     for(int j = 0; j <= width - n; j += stride) {
-      double filterValue = (type ? getAveragePixel(i, j, n, image)  : getMaxPixel(i, j, n, image));
+      // 根據 type 決定 average 或是 max
+      double filterValue = (!type ? getAveragePixel(i, j, n, image)  : getMaxPixel(i, j, n, image));
       colColor.push_back({filterValue, filterValue, filterValue});
     }
     resultVec.push_back(colColor);
@@ -65,9 +95,11 @@ Mat pool(Mat image, int n, int stride, int type) {
 
   Mat resultImage(resultVec.size(), resultVec[0].size(), CV_8UC3);
 
+  // 將 resultVec 的資訊塞到 resultImage 
   for(int i = 0; i < resultImage.rows; i++) {
     for(int j = 0; j < resultImage.cols; j++) {
       Vec3b &color = resultImage.at<Vec3b>(i, j);
+      // 取得 color 指標
       for(int k = 0; k < 3; k++) {
         color[k] = resultVec[i][j][k];
       }
@@ -93,14 +125,14 @@ int main() {
   }
 
   getImageInfo(image);
-  imshow("origin image", image);
 
   Mat resultImage = pool(image, n, step, type);
+  writeName(resultImage);
   getImageInfo(resultImage);
+  // imwrite("./result/pool_otters_maximum.png", resultImage);
   imshow("result image", resultImage);
 
   waitKey(0);
-
 
   return 0;
 }
